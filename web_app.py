c069b4baf8d0bd2a9988bc89ac9fafd0b191d1fd
Eric Metzler <metzlere@umich.edu>
2023-06-29 16:09:02 -0400

    fixes for df length mismatches between train df and user input df

--- web_app.py
+++ web_app.py
@@ -14,29 +14,26 @@ def predict(data, treatment, feature_names):
     # Add the treatment column to the DataFrame
     df['SERVICES_AT_ADMISSION'] = treatment
     
-    # Filter rows based on condition
-    df = df[df['REASON_FOR_DISCHARGE'] != 'Transferred to another treatment program or facility']
+    # # Filter rows based on condition
+    # df = df[df['REASON_FOR_DISCHARGE'] != 'Transferred to another treatment program or facility']
 
-    # Create target variable
-    df['SUCCESSFUL_TREATMENT'] = df.apply(lambda row: 1 if row['REASON_FOR_DISCHARGE'] == 'Treatment completed' and row['PRIOR_TREATMENT_EPISODES'] == "No prior treatment episode" else 0, axis=1)
+    # # Create target variable
+    # df['SUCCESSFUL_TREATMENT'] = df.apply(lambda row: 1 if row['REASON_FOR_DISCHARGE'] == 'Treatment completed' and row['PRIOR_TREATMENT_EPISODES'] == "No prior treatment episode" else 0, axis=1)
     
-    # Remove unnecessary columns
-    features = df.drop(['REASON_FOR_DISCHARGE', 'PRIOR_TREATMENT_EPISODES', 'SUCCESSFUL_TREATMENT'], axis=1)
+    # # Remove unnecessary columns
+    # features = df.drop(['REASON_FOR_DISCHARGE', 'PRIOR_TREATMENT_EPISODES', 'SUCCESSFUL_TREATMENT'], axis=1)
     
     # Perform one-hot encoding on the DataFrame
-    features_one_hot = pd.get_dummies(features)
+    features_one_hot = pd.get_dummies(df)    
 
     # Align DataFrame with the training DataFrame's columns
     df_aligned = features_one_hot.reindex(columns=feature_names, fill_value=0)
-
-    # Temporary fix for missing columns, need to determine why there is a mismatch
+    # Check for missing columns
     missing_columns = set(feature_names) - set(df_aligned.columns)
 
-    # Check for missing columns
-    st.write("Missing columns:", missing_columns)
-
-    for column in missing_columns:
-        df_aligned[column] = 0
+    # Temporary fix for missing columns, need to determine why there is a mismatch
+    # for column in missing_columns:
+    #     df_aligned[column] = 0
     
 
     # Convert the DataFrame to a numerical array
@@ -55,13 +52,23 @@ def main():
     # Read cleaned DF used to train the model
     df = pd.read_csv('cleaned_data.csv')
     treatments = df['SERVICES_AT_ADMISSION'].unique().tolist()
-    df = df.drop(columns=['SERVICES_AT_ADMISSION'])
+
+    # Preprocessing
+    df = df[df['REASON_FOR_DISCHARGE'] != 'Transferred to another treatment program or facility']
+    df = df.drop(['REASON_FOR_DISCHARGE', 'PRIOR_TREATMENT_EPISODES'], axis=1)
+    df['HEALTH_INSURANCE'] = df['HEALTH_INSURANCE'].fillna('None')
+    df['ARRESTS_IN_30_DAYS_PRIOR_TO_ADMISSION'] = df['ARRESTS_IN_30_DAYS_PRIOR_TO_ADMISSION'].fillna('None')
+    df['ARRESTS_IN_30_DAYS_PRIOR_TO_DISCHARGE'] = df['ARRESTS_IN_30_DAYS_PRIOR_TO_DISCHARGE'].fillna('None')
+
+    # Drop the treatment column - need to fix
+    #df = df.drop(columns=['SERVICES_AT_ADMISSION'])
     
     # Get the feature names from the training DataFrame
     feature_names = pd.get_dummies(df).columns
     
     # Create dictionary to hold user inputs
     user_inputs = {}
+
 
     # Populate user inputs with unique values from the df
     for column in df.columns:
@@ -70,6 +77,7 @@ def main():
 
         # Create a user input for the column
         user_inputs[column] = st.selectbox(column, unique_values)
+
 
     # Create a button and use it to run predictions for each treatment option
     if st.button("Predict"):
